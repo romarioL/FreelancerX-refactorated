4cbbe6d9d1962389271a692678a979c057e52473
Romis Sousa <estreladamanha585@gmail.com>
2020-07-13 10:15:40 -0300

    Template-parts/tag.php

--- tag.php
+++ tag.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <div class="banner-posts">
 	<?php if(have_posts()): while(have_posts()): the_post(); ?>
-	<?php get_template_part('template-parts/content', 'page'); ?>
+	<?php get_template_part('template-parts/content', get_post_format()); ?>
 <?php endwhile; endif; ?>
 </div>
 <?php get_footer(); ?>
